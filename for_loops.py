1c0a27d3af2c9e4a4c5a92b8104529e010788706
sam chris <vsgitsam25@gmail.com>
2025-11-17 10:55:55 +0300

    Worked on some loops and nested looping

---
+++ for_loops.py
@@ -0,0 +1,11 @@
+for item in range(5,100):
+    print(item)
+    
+    
+prices = [100,20,30]
+total=0    
+# calculating the total in a list    
+
+for price in prices:
+    total = total+price
+print(f'Total: {total}')    
---
+++ nested.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    for y in range(9):
+        for z in range(8):
+            print(f'({i},{y},{z})')
+    
+    
+    
