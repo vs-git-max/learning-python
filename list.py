56593b494032c19ed3084a2055b178f31e8b5024
sam chris <vsgitsam25@gmail.com>
2025-11-17 12:17:54 +0300

    working on list

---
+++ list.py
@@ -0,0 +1,13 @@
+names = ['Sam', 'Chris', 'Mboya', 'Phone', 'Rose']
+
+print(names[:])
+
+
+numbers = [1,2,3,4,56,7,8,9,10]
+
+max = numbers[0]
+print(max)
+for number in numbers:
+    if number > max:
+        max = number
+print(max)        
